d73b3723b950fa4e7b121e673d0cc792608f71e4
Owen Healy <owen@owenehealy.com>
2021-06-18 08:14:20 -0400

    enum for more like tmk

--- keys.h
+++ keys.h
@@ -10,20 +10,20 @@ extern "C" {
 
 #define MAX_KEYS 12
 
-typedef uint8_t key_code;
-
-#define A 0
-#define B 1
-#define C 2
-#define D 3
-#define LEFTSHIFT 4
-#define RIGHTSHIFT 5
-#define LEFTCTRL 6
-#define RIGHTCTRL 7
-#define LEFTALT 8
-#define RIGHTALT 9
-#define LEFTMETA 10
-#define RIGHTMETA 11
+typedef enum {
+  A = 0,
+  B,
+  C,
+  D,
+  LEFTSHIFT,
+  RIGHTSHIFT,
+  LEFTCTRL,
+  RIGHTCTRL,
+  LEFTALT,
+  RIGHTALT,
+  LEFTMETA,
+  RIGHTMETA,
+} key_code;
 
 #ifdef __cplusplus
 }
