fc9a8d1135561cc3f64cde978e21dbbdafe82e30
Owen Healy <owen@owenehealy.com>
2021-06-20 11:09:21 -0400

    TMK names

--- tests/test-1.c
+++ tests/test-1.c
@@ -1,34 +1,34 @@
 
-#include "keys.h"
-#include "mapping.h"
-#include "debugging.h"
+#include "../keys.h"
+#include "../mapping.h"
+#include "../debugging.h"
 #include <stdio.h>
 
 static const struct mapping our_mappings_A[] = {
-  { .from_modifiers = 0x0, .from_absorbing_modifiers = 0x0, .to_modifiers = 0x0, .to_action = B },
-  { .from_modifiers = MOD1_MASK, .from_absorbing_modifiers = 0x0, .to_modifiers = LEFT_SHIFT_MASK, .to_action = B },
+  { .from_modifiers = 0x0, .from_absorbing_modifiers = 0x0, .to_modifiers = 0x0, .to_action = KC_B },
+  { .from_modifiers = MOD1_MASK, .from_absorbing_modifiers = 0x0, .to_modifiers = LEFT_SHIFT_MASK, .to_action = KC_B },
 };
   
 static const struct mapping our_mappings_B[] = {
-  { .from_modifiers = 0x0, .from_absorbing_modifiers = 0x0, .to_modifiers = 0x0, .to_action = C },
-  { .from_modifiers = MOD1_MASK, .from_absorbing_modifiers = 0x0, .to_modifiers = LEFT_SHIFT_MASK, .to_action = C },
+  { .from_modifiers = 0x0, .from_absorbing_modifiers = 0x0, .to_modifiers = 0x0, .to_action = KC_C },
+  { .from_modifiers = MOD1_MASK, .from_absorbing_modifiers = 0x0, .to_modifiers = LEFT_SHIFT_MASK, .to_action = KC_C },
 };
   
 static const struct mapping our_mappings_C[] = {
-  { .from_modifiers = 0x0, .from_absorbing_modifiers = 0x0, .to_modifiers = 0x0, .to_action = A },
-  { .from_modifiers = MOD1_MASK, .from_absorbing_modifiers = MOD1_MASK, .to_modifiers = LEFT_SHIFT_MASK, .to_action = A },
+  { .from_modifiers = 0x0, .from_absorbing_modifiers = 0x0, .to_modifiers = 0x0, .to_action = KC_A },
+  { .from_modifiers = MOD1_MASK, .from_absorbing_modifiers = MOD1_MASK, .to_modifiers = LEFT_SHIFT_MASK, .to_action = KC_A },
 };
   
 static const struct mapping our_mappings_D[] = {
-  { .from_modifiers = 0x0, .from_absorbing_modifiers = 0x0, .to_modifiers = LEFT_CTRL_MASK, .to_action = A },
+  { .from_modifiers = 0x0, .from_absorbing_modifiers = 0x0, .to_modifiers = LEFT_CTRL_MASK, .to_action = KC_A },
 };
 
 static const struct key_definition our_key_definitions[] = {
-  [A] = { action_key_style, .action_key = { .num_mappings = 2, .mappings = our_mappings_A } },
-  [B] = { action_key_style, .action_key = { .num_mappings = 2, .mappings = our_mappings_B } },
-  [C] = { action_key_style, .action_key = { .num_mappings = 2, .mappings = our_mappings_C } },
-  [D] = { action_key_style, .action_key = { .num_mappings = 1, .mappings = our_mappings_D } },
-  [LEFTSHIFT] = { modifier_key_style, .modifier_key = { MOD1_MASK, LEFT_SHIFT_MASK } },
+  [KC_A] = { action_key_style, .action_key = { .num_mappings = 2, .mappings = our_mappings_A } },
+  [KC_B] = { action_key_style, .action_key = { .num_mappings = 2, .mappings = our_mappings_B } },
+  [KC_C] = { action_key_style, .action_key = { .num_mappings = 2, .mappings = our_mappings_C } },
+  [KC_D] = { action_key_style, .action_key = { .num_mappings = 1, .mappings = our_mappings_D } },
+  [KC_LSHIFT] = { modifier_key_style, .modifier_key = { MOD1_MASK, LEFT_SHIFT_MASK } },
 };
 
 static const struct layout our_layout = {
@@ -65,25 +65,25 @@ static void test(struct state *state, enum event_type t, key_code k) {
 int main() {
   struct state state = { };
   
-  test(&state, PRESSED, A);
-  test(&state, PRESSED, LEFTSHIFT);
-  test(&state, PRESSED, B);
-  test(&state, RELEASED, LEFTSHIFT);
-  test(&state, RELEASED, B);
-  test(&state, RELEASED, A);
-  test(&state, PRESSED, D);
-  test(&state, PRESSED, C);
-  test(&state, RELEASED, C);
-  test(&state, PRESSED, LEFTSHIFT);
-  test(&state, PRESSED, C);
-  test(&state, PRESSED, B);
-  test(&state, RELEASED, C);
-  test(&state, RELEASED, B);
-  test(&state, RELEASED, LEFTSHIFT);
-  test(&state, PRESSED, LEFTSHIFT);
-  test(&state, PRESSED, C);
-  test(&state, RELEASED, C);
-  test(&state, PRESSED, C);
-  test(&state, RELEASED, C);
+  test(&state, PRESSED, KC_A);
+  test(&state, PRESSED, KC_LSHIFT);
+  test(&state, PRESSED, KC_B);
+  test(&state, RELEASED, KC_LSHIFT);
+  test(&state, RELEASED, KC_B);
+  test(&state, RELEASED, KC_A);
+  test(&state, PRESSED, KC_D);
+  test(&state, PRESSED, KC_C);
+  test(&state, RELEASED, KC_C);
+  test(&state, PRESSED, KC_LSHIFT);
+  test(&state, PRESSED, KC_C);
+  test(&state, PRESSED, KC_B);
+  test(&state, RELEASED, KC_C);
+  test(&state, RELEASED, KC_B);
+  test(&state, RELEASED, KC_LSHIFT);
+  test(&state, PRESSED, KC_LSHIFT);
+  test(&state, PRESSED, KC_C);
+  test(&state, RELEASED, KC_C);
+  test(&state, PRESSED, KC_C);
+  test(&state, RELEASED, KC_C);
 }
 
